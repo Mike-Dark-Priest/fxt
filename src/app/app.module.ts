ce3e6e6c4293a93f3f5caeeffa86da8c03ac83eb
Mike <mike.mbewe@standardbank.co.mw>
2024-08-16 14:51:16 +0200

    added auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Amplify } from 'aws-amplify';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+import awsconfig from '../aws-exports';
+import { environment } from '../environments/environment.prod'; 
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolClientId: environment.userPoolClientId,
+      userPoolId: environment.userPoolId,
+      loginWith: { // Optional
+        oauth: {
+          domain: 'abcdefghij1234567890-29051e27.auth.eu-north-1.amazoncognito.com',
+          scopes: ['openid email phone profile aws.cognito.signin.user.admin '],
+          redirectSignIn: ['http://localhost:4000/','https://example.com/'],
+          redirectSignOut: ['http://localhost:4000/','https://example.com/'],
+          responseType: 'code',
+        }
+        // username: 'true', 
+        // email: 'false', // Optional
+        // phone: 'false', // Optional
+      }
+    }
+  }
+});
 
 @NgModule({
   declarations: [
@@ -10,7 +35,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AmplifyAuthenticatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  identityPoolId: 'eu-north-1:284f983c-bbf4-473b-a509-7082c70b0cd9',  // Replace with your actual identity pool ID
+  region: 'eu-north-1',  // Replace with your AWS region
+  userPoolId: 'eu-north-1_N2Gvgespw',  // Replace with your actual user pool ID
+  userPoolWebClientId: '5bja1vb9g1mgms8cte1p0bilok',  // Replace with your actual user pool client ID
+  userPoolClientId: '7vns5f7ibm927g4pbc24sl0peg'
+
 };
